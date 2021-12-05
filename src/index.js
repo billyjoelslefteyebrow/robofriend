920ea2fa9869509a6cb9fffa55faf6feccd22801
Joelbrows <allisongraddy@yahoo.com>
2021-12-05 14:02:32 -0600

    First Commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import CardList from './components/CardList';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
+
+
+ReactDOM.render(
+<React.StrictMode>
+      <App />
+</React.StrictMode>, 
+document.getElementById('root')
+);
+
+
+
+reportWebVitals();
